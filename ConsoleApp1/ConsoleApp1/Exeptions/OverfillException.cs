ebbc24b9a4005fc8206804b2f40adae1b2d3cc5f
Wojciech Fuśnik <s24618@pjwstk.edu.pl>
2024-03-18 18:29:41 +0100

    Lesson-end

---
+++ ConsoleApp1/ConsoleApp1/Exeptions/OverfillException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Exeptions
+{
+    internal class OverfillException: Exception
+    {
+    }
+}
